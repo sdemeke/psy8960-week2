05fd3e3e25314a87faddb76c8a1c7f36aaa8df06
Saron M Demeke <demek004@umn.edu>
2023-01-25 16:25:31 -0600

    Finished through Line 9

--- R/week2.R
+++ R/week2.R
@@ -1,2 +1,10 @@
 #Import and Labeling
 rt_df <- read.csv(file = "./data/week2.csv", header = TRUE)
+rt_df$condition <- factor(rt_df$condition, levels = c("A","B"), labels = c("Control","Experimental"))
+rt_df$gender <- factor(rt_df$gender, levels = c("M","F","N"), labels = c("Male","Female","Nonbinary"))
+
+#Analysis
+mean(rt_df$rt)
+rt_f_df <- subset(rt_df, gender == "Female")
+hist(rt_f_df$rt)
+
